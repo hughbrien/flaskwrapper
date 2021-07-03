f0fe9749bc568df0c0941a4ef53cbe095c584e68
Hugh Brien <hugh@hughbrien.com>
2021-07-03 15:43:41 -0400

    Last push before branch

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # flaskwrapper
+
+
+Create Virtual Environment -
+
+    python3 -m venv my_venv 
+    source ./venv/bin/activate 
+    pip3 install -r ./requirements.txt
+
+    ./venv/bin/python -m flask run
+    flask run 
+
+
+Deactivate - To deactivate the virtual environment. 
+    
+    deactivate
+
+
+Now run the project. 
+
+
+
+
+
+
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -15,6 +15,14 @@ def list_service():
 def rollback_service():
     rollback_result = {"rollback":"cheddar", "version":"v2.2.3"}
     return rollback_result
+
+@app.route('/users/<user>')
+def users(user):
+   return render_template('user.html', name = user)
+
+@app.route('/servers/json/<server>')
+def servers(server):
+   return render_template('servers.json', name = server)
 
 if __name__ == '__main__':
     app.run()
