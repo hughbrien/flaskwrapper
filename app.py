6af4a2f10c1a8dad58ded007920c683b1c933fda
Hugh Brien <hugh.brien@gremlin.com>
2022-09-15 11:05:08 -0400

    fixed CR bug in file

--- app.py
+++ app.py
@@ -7,22 +7,13 @@ app = Flask(__name__)
 @app.route('/')
 def list_service():
 
-    service_list = {"services":[
-        "service-list":"/services/list",
-        "rollback_service":"/rollback_service'",
-        "/users/<user>":"/users/<user>",
-        "/servers/json":"/servers/json",
-    ]
-     }
+    service_list = {"item":"items"}
     return service_list
 
-@app.route('/services/list')
-def list_service():
+@app.route('/demo')
+def demo():
 
-    service_list = {"services":[
-        "cart","demo","extra","cheddar"
-    ]
-     }
+    service_list = {"item":"items"}
     return service_list
 
 @app.route('/rollback_service')
